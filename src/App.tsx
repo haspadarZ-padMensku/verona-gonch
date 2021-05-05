1c127dd49680cf367355a23f8fe96cead8dd7e44
Denis Goncharov <gonchds@gmail.com>
2021-05-06 00:10:38 +0300

    add routing; use absolute paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Home = lazy(() => import("routes/Home"));
+const About = lazy(() => import("routes/About"));
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Suspense>
+  </Router>
+);
 
 export default App;
